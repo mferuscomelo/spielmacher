a544f4bdb7d1927db6b4f27daa8cf1f686255f89
Milan Ferus-Comelo <51026229+mferuscomelo@users.noreply.github.com>
2025-01-20 09:20:14 +0000

    Add callback for running tournament

--- src/spielmacher/tournament.py
+++ src/spielmacher/tournament.py
@@ -1,4 +1,5 @@
-from spielmacher.game import Game
+from typing import Callable, Optional
+from spielmacher.game import Game, GameConfig
 from spielmacher.player import Player
 
 
@@ -10,19 +11,18 @@ class Tournament:
         self.__players = players
         self.__rounds_per_game = rounds_per_game
 
-    def run(self) -> None:
+    def run(self, callback: Optional[Callable[[Player, Player], None]] = None) -> None:
         for i in range(len(self.__players)):
             for j in range(i + 1, len(self.__players)):
                 p1 = self.__players[i]
                 p2 = self.__players[j]
 
-                game = Game(p1, p2, self.__rounds_per_game)
-                winner = game.run()
-                scores = game.get_scores()
+                config = GameConfig(p1, p2, self.__rounds_per_game)
+                game = Game(config)
+                game.run()
 
-                print(f"{p1.name} vs {p2.name}: {scores}")
-                p1.print_moves()
-                p2.print_moves()
-                print(f"Winner: {winner.name}")
+                if callback is not None:
+                    callback(p1, p2)
 
-                game.plot_scores()
+                p1.prepare_for_next_game()
+                p2.prepare_for_next_game()
